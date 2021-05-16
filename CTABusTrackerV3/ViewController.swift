4d72a511e8f387ead97e5dbff79ad1d1130a48ff
DewoneW536 <dwesterf@mail.depaul.edu>
2021-05-16 13:49:56 -0500

    Initial Commit

---
+++ CTABusTrackerV3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CTABusTrackerV3
+//
+//  Created by Dewone Westerfield on 5/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CTABusTrackerV3Tests/CTABusTrackerV3Tests.swift
@@ -0,0 +1,33 @@
+//
+//  CTABusTrackerV3Tests.swift
+//  CTABusTrackerV3Tests
+//
+//  Created by Dewone Westerfield on 5/16/21.
+//
+
+import XCTest
+@testable import CTABusTrackerV3
+
+class CTABusTrackerV3Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
